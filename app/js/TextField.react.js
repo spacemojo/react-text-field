c4ce8d70650efef711900c9af8752073b0374a96
Fred Leclerc <fred.leclerc@gmail.com>
2016-11-17 12:50:46 -0500

    This seems to work ...

--- app/js/TextField.react.js
+++ app/js/TextField.react.js
@@ -13,10 +13,12 @@ let TextField = React.createClass({
 
   componentDidMount: function() {
     this.currentValue = (this.props.value == undefined) ? "" : this.props.value;
+    document.getElementById(this.id).value = this.currentValue;
   },
 
   componentWillReceiveProps: function(newProps) {
     this.currentValue = (newProps.value == undefined) ? "" : newProps.value;
+    document.getElementById(this.id).value = this.currentValue;
   },
 
   onChange: function(event) {
@@ -28,7 +30,7 @@ let TextField = React.createClass({
     return (
       <div>
         <label htmlFor={this.id}>{this.props.name}</label>
-        <input style={{color: "#000000"}} type="text" className="u-full-width" placeholder={this.props.name} id={this.id} defaultValue={this.currentValue} onChange={this.onChange} />
+        <input style={{color: "#000000"}} type="text" className="u-full-width" placeholder={this.props.name} id={this.id} onChange={this.onChange} />
       </div>
     )
 
--- index.js
+++ index.js
@@ -7,8 +7,18 @@ let TextField = React.createClass({displayName: "TextField",
 
   getInitialState: function() {
     this.id = UUID.v4();
-    this.currentValue = (this.props.value == undefined) ? "" : this.props.value;
+    this.currentValue = "";
     return null;
+  },
+
+  componentDidMount: function() {
+    this.currentValue = (this.props.value == undefined) ? "" : this.props.value;
+    document.getElementById(this.id).value = this.currentValue;
+  },
+
+  componentWillReceiveProps: function(newProps) {
+    this.currentValue = (newProps.value == undefined) ? "" : newProps.value;
+    document.getElementById(this.id).value = this.currentValue;
   },
 
   onChange: function(event) {
@@ -20,7 +30,7 @@ let TextField = React.createClass({displayName: "TextField",
     return (
       React.createElement("div", null, 
         React.createElement("label", {htmlFor: this.id}, this.props.name), 
-        React.createElement("input", {style: {color: "#000000"}, type: "text", className: "u-full-width", placeholder: this.props.name, id: this.id, defaultValue: this.currentValue, onChange: this.onChange})
+        React.createElement("input", {style: {color: "#000000"}, type: "text", className: "u-full-width", placeholder: this.props.name, id: this.id, onChange: this.onChange})
       )
     )
 
